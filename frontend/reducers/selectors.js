dce4cc495ef1f8370ad8d9af69740d1d32084157
Jonathan Lin <jclin2013@gmail.com>
2017-04-21 21:57:42 -0400

    Refactor notes selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -7,22 +7,30 @@ export const notesSelector = (notesObject) => {
   let sorted_arr = Object.values(notesObject).sort(dateComparator)
 
   sorted_arr.forEach(noteObj => {
-    let time_diff = Math.floor(
-      (Date.now() - new Date(noteObj['updated_at']))/1000
-    )
+    let seconds = Math.floor((Date.now() - new Date(noteObj['updated_at']))/1000),
+        minutes = Math.floor(seconds/60),
+        hours = Math.floor(minutes/60),
+        days = Math.floor(hours/24)
 
-    if (time_diff < 60) {
-        noteObj['time_passed'] = time_diff + " seconds ago";
-    } else if (Math.floor(time_diff/60) < 60) {
-        noteObj['time_passed'] = Math.floor(time_diff/60) + " minutes ago";
-    } else if (Math.floor(time_diff/60/60) < 24) {
-        noteObj['time_passed'] = Math.floor(time_diff/60/60) + " hours ago";
-    } else if (Math.floor(time_diff/60/60/24) < 8) {
-        noteObj['time_passed'] = Math.floor(time_diff/60/60/24) + " days ago";
-      }
-      else {
+    if (seconds < 2) {
+        noteObj['time_passed'] = seconds + " second ago";
+    } else if (seconds < 60) {
+        noteObj['time_passed'] = seconds + " seconds ago";
+    } else if (minutes < 2) {
+        noteObj['time_passed'] = minutes + " minute ago";
+    } else if (minutes < 60) {
+        noteObj['time_passed'] = minutes + " minutes ago";
+    } else if (hours < 2) {
+        noteObj['time_passed'] = hours + " hour ago";
+    } else if (hours < 24) {
+        noteObj['time_passed'] = hours + " hours ago";
+    } else if (days < 2) {
+        noteObj['time_passed'] = days + " day ago";
+    } else if (days < 8) {
+        noteObj['time_passed'] = days + " days ago";
+    } else {
         noteObj['time_passed'] = new Date(noteObj['updated_at']).toLocaleDateString();
-      }
+    }
   });
 
   return sorted_arr;

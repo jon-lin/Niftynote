ae9ffc269f525800946745348788ecdf4df1fd1f
Jonathan Lin <jclin2013@gmail.com>
2017-04-25 09:21:28 -0400

    Finish note info modale page

--- frontend/components/notes/note_info_page.jsx
+++ frontend/components/notes/note_info_page.jsx
@@ -8,10 +8,18 @@ class NoteInfoPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.note.title}</h1>
+
+        <div className="noteInfoHeader3">
+          <figure>
+            <i className="fa fa-info-circle" aria-hidden="true"></i>
+            <text>NOTE INFO</text>
+          </figure>
+            <h1>{this.props.note.title}</h1>
+        </div>
+
          <ul>
-           <li>{this.props.note.created_at}</li>
-           <li>{this.props.note.updated_at}</li>
+           <li>CREATED: {(new Date(this.props.note.created_at).toLocaleString())}</li>
+           <li>UPDATED: {(new Date(this.props.note.updated_at).toLocaleString())}</li>
          </ul>
          <button onClick={this.props.closeModal}>CLOSE</button>
       </div>

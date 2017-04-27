127072e9fc6b9a38c3a415b2e7a6ad2d9ae44efd
Jonathan Lin <jclin2013@gmail.com>
2017-04-27 12:05:27 -0400

    Fix json return for create notebook

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -8,7 +8,7 @@ class Api::NotebooksController < ApplicationController
     @notebook.author_id = current_user.id
 
     if @notebook.save
-      render json: [@notebook.title + ' saved!']
+      render :show
     else
       errors = @notebook.errors.full_messages
       render json: errors, status: 422

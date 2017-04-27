c2ce95b3c6fa7ef411b97aa15771229d0033c36a
Jonathan Lin <jclin2013@gmail.com>
2017-04-27 16:24:55 -0400

    Moving past failed attempt to add flexibility to notebook update controller method

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -29,17 +29,34 @@ class Api::NotebooksController < ApplicationController
   def update
     @notebook = current_user.notebooks.find(params[:id])
 
-    # if the user is looking just to update the defaultNotebook status, maybe allow it with the following?
-    # if (params[:notebook][:title] == "") && (params[:notebook][:defaultNotebook] == "true") && (@notebook.title != "")
-    #   notebook_params[:title] = @notebook.title
+    # intermediary code here allows default to be set
+    # even if no title was entered; if no title is entered;
+    # the existing title of the notebook is passed in.
+
+    # otherwise, if a new title AND default notebook are passed in,
+    # both of them are used.
+
+    #if just a title is passed in, that will also work
+
+    # in both cases, the existing default notebook toggles to not being the
+    # default, using the prev_default stuff
+
+    # if @notebook && (params[:notebook][:title] == "") && (params[:notebook][:defaultNotebook] == "true")
+    #   mod_params = {
+    #                 title: @notebook.title,
+    #                 defaultNotebook: true
+    #               }
+    # else
+    #   mod_params = notebook_params
     # end
     #
-    if params[:notebook][:defaultNotebook] == "true"
-      prev_default = current_user.notebooks.find_by_defaultNotebook(true)
-      prev_default.update({defaultNotebook: false})
-    end
+    # prev_default = current_user.notebooks.find_by_defaultNotebook(true)
 
+    # if @notebook.update(mod_params)
     if @notebook.update(notebook_params)
+      # if prev_default && (mod_params[:defaultNotebook] == true)
+      #   prev_default.update({defaultNotebook: false})
+      # end
       render :show
     else
       errors = @notebook.errors.full_messages

00a9483149efc8f9ce4c758a1f5361ef60701716
Jonathan Lin <jclin2013@gmail.com>
2017-04-28 17:01:43 -0400

    Rearrange CSS files, rename new note files

--- frontend/components/notes/new_note.jsx
+++ frontend/components/notes/new_note.jsx
@@ -1,7 +1,8 @@
 import ReactQuill from 'react-quill';
 import React from 'react';
-import { createNote } from '../../actions/notes_actions';
+import { createNote, updateNote } from '../../actions/notes_actions';
 import { connect } from 'react-redux';
+import NotebookScrollbar from '../notebooks/notebook_scrollbar';
 import { withRouter } from 'react-router';
 
 class NewNote extends React.Component {
@@ -9,10 +10,52 @@ class NewNote extends React.Component {
 
     super(props)
 
-    this.state = { body: '', title: '', }
+    let delayTimerCreate = () => setTimeout(() => {
+      return this.props.createNote(
+            {
+              body: this.state.body,
+              title: this.state.title,
+              notebookId: this.state.notebookId
+            }
+          )
+    }, 1000)
+
+    let delayTimerUpdate = () => setTimeout(() => {
+      return this.props.updateNote(
+            {
+              body: this.state.body,
+              title: this.state.title,
+            }
+          )
+    }, 1000)
+
+    this.state = { body: '',
+                  title: '',
+                  delayTimer: delayTimer,
+                  timerId: delayTimer(),
+                  notebookId: 0
+                 }
+    // this.handleChange = this.handleChange.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.submitNewNote = this.submitNewNote.bind(this);
+    this.leaveNewNotePage = this.leaveNewNotePage.bind(this);
   }
+
+  // componentDidMount() {
+  //
+  //   debugger
+  //   let defaultNotebookId = this.props.notebooks.filter( (notebookObj) => {
+  //     return notebookObj.defaultNotebook === true;
+  //   })[0].id;
+  //
+  //   debugger
+  //
+  //   this.setState({notebookId: defaultNotebookId});
+  // }
+
+  // componentWillReceiveProps(newProps) {
+  //   debugger
+  //   this.setState({body: this.state.body, title: this.state.title})
+  // }
 
   handleInputChange(event) {
     let param;
@@ -22,15 +65,14 @@ class NewNote extends React.Component {
       param = {title: event.target.value};
     }
 
-    console.log(this.state);
+    console.log(this.state)
 
-    this.setState(param);
+    this.setState(param, clearTimeout(this.state.timerId));
+    this.setState({timerId: this.state.delayTimer()});
   }
 
-
-  submitNewNote() {
-    this.props.createNote(this.state)
-      .then(() => this.props.router.push('/home'));
+  leaveNewNotePage() {
+    this.props.router.push('/home');
   }
 
   render() {
@@ -54,27 +96,33 @@ class NewNote extends React.Component {
           ['clean']                                         // remove formatting button
         ];
 
+    let formType = (this.props.location.pathname === '/home') ? 'homeDropDown' : 'newNotebookDropDown';
 
     return (
-      <div className="quill2bigContainer">
-        <div className="quillContainer2">
-          <input placeholder="Type title in here" type="text" value={this.state.title} onChange={this.handleInputChange}></input>
-          <ReactQuill value={this.state.body}
-                      onChange={this.handleInputChange}
-                      modules={ {toolbar: toolbarOptions} }/>
+      <div className="quillContainer">
 
-                    <button onClick={this.submitNewNote}>Save New Note</button>
-                    <button onClick={() => this.props.router.push('/home')}>Cancel</button>
-        </div>
-      </div>
+        <NotebookScrollbar formType={formType}/>
+        <input type="text" value={this.state.title} onChange={this.handleInputChange}></input>
+        <ReactQuill value={this.state.body}
+                    onChange={this.handleInputChange}
+                    modules={ {toolbar: toolbarOptions} }/>
+
+                  <button onClick={this.leaveNewNotePage}>Done</button>
+    </div>
     )
   }
 }
 
+const mapStateToProps = (state) => {
+  return { notebooks: Object.values(state.notebooks) };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
-    createNote: (note) => dispatch(createNote(note))
+    createNote: (note) => dispatch(createNote(note)),
+    updateNote: (note) => dispatch(updateNote(note))
+    // fetchNotebooks: (notebooks) => dispatch(fetchNotebooks(notebooks))
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(NewNote));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NewNote));

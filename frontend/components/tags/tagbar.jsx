2e47d433430c673f38018a067952e1d736936743
Jonathan Lin <jclin2013@gmail.com>
2017-05-14 13:04:46 -0400

    Get tag creation/deletion on homepage working

--- frontend/components/tags/tagbar.jsx
+++ frontend/components/tags/tagbar.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createTags } from '../../actions/tags_actions';
 
 class Tagbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tags: []
+      tags: [],
+      newTag: ""
     }
+    this.removeTag = this.removeTag.bind(this);
+    this.addNewTag = this.addNewTag.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
-    this.state.tags = newProps.currentNote.tags
+    this.state.tags = newProps.currentNote.tags;
+  }
+
+  removeTag(e) {
+    let updatedTags = this.state.tags.filter(tag => {
+      return tag.id !== parseInt(e.target.value);
+    });
+
+    let updateTagsJustNames = updatedTags.map(tag => tag.name);
+
+    this.props.createTags(updateTagsJustNames, this.props.currentNote.id);
+  }
+
+  addNewTag(e) {
+    if (e.keyCode === 13) {
+      let copyTags = JSON.parse(JSON.stringify(this.state.tags));
+      copyTags.push({name: this.state.newTag});
+
+      let copyTagsJustNames = copyTags.map(tag => tag.name);
+
+      this.props.createTags(copyTagsJustNames, this.props.currentNote.id)
+        .then(() => this.setState({newTag: ''}));
+    }
+  }
+
+  handleChange(e) {
+    this.setState({newTag: e.target.value});
   }
 
   render() {
 
     let placeholderText = 'New tag...';
-    let formatTags = this.state.tags.map(tag => <li>{tag.name}</li>);
-    if (formatTags.length !== 0) { placeholderText = '+'; }
+
+    let formatTags = this.state.tags.map(tag =>
+      <li key={tag.id}>
+        {tag.name}
+        <button value={tag.id} onClick={this.removeTag}>x</button>
+      </li>
+    );
+
+    if (formatTags.length !== 0) { placeholderText = ' + '; }
 
     return (
       <div className='entireTagBar'>
@@ -25,7 +63,12 @@ class Tagbar extends React.Component {
         <ul className='existingTagsList'>
           {formatTags}
         </ul>
-        <input placeholder={placeholderText}></input>
+        <input
+          onChange={this.handleChange}
+          onKeyDown={this.addNewTag}
+          value={this.state.newTag}
+          placeholder={placeholderText}>
+        </input>
       </div>
     )
   }
@@ -39,6 +82,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    createTags: (names, noteId) => dispatch(createTags(names, noteId))
   };
 };
 

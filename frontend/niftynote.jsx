4f7883617d08fc7027c5eb2bc77021373feeab26
Jonathan Lin <jclin2013@gmail.com>
2017-08-09 20:13:41 -0400

    Remove tags action testing on window

--- frontend/niftynote.jsx
+++ frontend/niftynote.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import * as TagsActions from './actions/tags_actions.js';
-
-window.fetchAllTags = TagsActions.fetchAllTags;
-window.updateTag = TagsActions.updateTag;
-window.createTags = TagsActions.createTags;
-window.deleteTag = TagsActions.deleteTag;
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;

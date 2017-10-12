c7ea7b26de32bd14f292c79bbc5beb6fd865eb33
Jonathan Lin <jclin2013@gmail.com>
2017-10-12 18:57:28 -0400

    Separate out dependencies for dev and prod, update store.js and webpack.config.js

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger'
 
-const configureStore = (preloadedState = {}) => {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-  )
-};
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares))
+);
 
 export default configureStore;

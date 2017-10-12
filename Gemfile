2e658c65e70d0187ecc1f4500a221443851917cc
Jonathan Lin <jclin2013@gmail.com>
2017-10-12 18:25:29 -0400

    Add google analytics

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ end
 
 gem 'faker'
 
+gem 'google-analytics-rails'
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  GA.tracker = 'UA-108073834-1'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
